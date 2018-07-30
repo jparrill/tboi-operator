d20401d9e63a697be2088244be454eb60168aa0c
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2018-07-30 22:53:44 +0200

    INITIAL COMMIT

---
+++ pkg/apis/tboi/v1alpha1/types.go
@@ -0,0 +1,29 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type ItemList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []Item `json:"items"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type Item struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+	Spec              ItemSpec   `json:"spec"`
+	Status            ItemStatus `json:"status,omitempty"`
+}
+
+type ItemSpec struct {
+	// Fill me
+}
+type ItemStatus struct {
+	// Fill me
+}
