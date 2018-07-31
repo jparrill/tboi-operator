feb4d8561cae9f129a90ebc062cc53cfb3ba268a
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2018-07-31 16:29:12 +0200

    finised working the DC with readiness and liveness probes and implemented SVC

--- pkg/stub/handler.go
+++ pkg/stub/handler.go
@@ -10,6 +10,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	//schema "k8s.io/apimachinery/pkg/runtime/schema"
 )
 
@@ -20,12 +21,12 @@ func NewHandler() sdk.Handler {
 type Handler struct{}
 
 func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
-	logrus.Debug("Start Handling")
+	logrus.Infof("Start Handling")
 	switch o := event.Object.(type) {
 	case *v1alpha1.Item:
 		tboi := o
-		logrus.Debug("Switch Object: %s", tboi)
-		logrus.Debug("Handler: %s", h)
+		logrus.Infof("Switch Object: %s", tboi)
+		logrus.Infof("Handler: %s", h)
 
 		// Create the deployment if it doesn't exist
 		dc := dcTboiItems(tboi)
@@ -34,7 +35,15 @@ func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
 			return fmt.Errorf("failed to create deployment: %v", err)
 		}
 
-		logrus.Debug("Finish Handling")
+		// Create the svc if it doesn't exist
+		svc := svcTboiItems(tboi)
+		err = sdk.Create(svc)
+		if err != nil && !apierrors.IsAlreadyExists(err) {
+			logrus.Errorf("Failed to create service : %v", err)
+			return err
+		}
+
+		logrus.Infof("Finish Handling")
 	}
 	return nil
 }
@@ -46,7 +55,7 @@ func getPodLabels(name string) map[string]string {
 }
 
 func dcTboiItems(h *v1alpha1.Item) *appsv1.Deployment {
-	logrus.Debug("Making dc spec")
+	logrus.Infof("Making dc spec")
 	labels := getPodLabels(h.Name)
 	replicas := h.Spec.ItemSize
 	dc := &appsv1.Deployment{
@@ -69,8 +78,35 @@ func dcTboiItems(h *v1alpha1.Item) *appsv1.Deployment {
 				},
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{{
-						Image: "docker.io/padajuan/tboi-operator",
+						Image: "docker.io/padajuan/tboi-operator-app",
 						Name:  labels["app"],
+						LivenessProbe: &corev1.Probe{
+							Handler: corev1.Handler{
+								HTTPGet: &corev1.HTTPGetAction{
+									Path: "/",
+									Port: intstr.IntOrString{Type: intstr.Int, IntVal: 5000},
+								},
+							},
+							InitialDelaySeconds: 3,
+							PeriodSeconds:       3,
+							TimeoutSeconds:      3,
+							FailureThreshold:    3,
+							SuccessThreshold:    1,
+						},
+						ReadinessProbe: &corev1.Probe{
+							Handler: corev1.Handler{
+								HTTPGet: &corev1.HTTPGetAction{
+									Path: "/",
+									Port: intstr.IntOrString{Type: intstr.Int, IntVal: 5000},
+								},
+							},
+							InitialDelaySeconds: 3,
+							PeriodSeconds:       3,
+							TimeoutSeconds:      3,
+							FailureThreshold:    3,
+							SuccessThreshold:    3,
+						},
+
 						Ports: []corev1.ContainerPort{{
 							ContainerPort: 5000,
 							Name:          labels["app"],
@@ -80,6 +116,35 @@ func dcTboiItems(h *v1alpha1.Item) *appsv1.Deployment {
 			},
 		},
 	}
-	logrus.Debug("Spec Finished")
+	logrus.Infof("DC: %s", dc)
+	logrus.Infof("DC Spec Finished")
 	return dc
+}
+
+func svcTboiItems(h *v1alpha1.Item) *corev1.Service {
+	logrus.Infof("Making svc Spec")
+	labels := getPodLabels(h.Name)
+	svc := &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Service",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      h.Name,
+			Namespace: h.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Type:     corev1.ServiceTypeLoadBalancer,
+			Selector: labels,
+			Ports: []corev1.ServicePort{
+				{
+					Name: "http",
+					Port: 5000,
+				},
+			},
+		},
+	}
+	logrus.Infof("SVC: %s", svc)
+	logrus.Infof("SVC Spec Finished")
+	return svc
 }
